31151fef93080067945d5c4564c47fda992c58d6
IHomov <innapariy@gmail.com>
2022-09-19 20:53:42 +0200

    link
    added v-bind for link

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+   data() {
+      return {
+         courseGoal: "Finish the course and lear Vue!",
+         vueLink: 'https://vuejs.org/'
+
+      };
+   }
+});
+
+app.mount('#user-goal');
